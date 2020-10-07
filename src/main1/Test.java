b495d790a4e2ce22042476ccef8dbc526dfd1b36
GUIORGUO/GUIORGUO <847666493@qq.com>
2020-10-07 11:26:12 +0800

    guoqi

---
+++ src/main1/Test.java
@@ -0,0 +1,30 @@
+package main1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Test {
+
+	public static void ms() {
+		// TODO Auto-generated method stub
+		List<Integer> a =new ArrayList();
+		a.add(20);
+		a.add(50);
+		a.add(70);
+		a.add(60);
+		a.add(50);
+		a.add(50);
+		a.add(90);
+		a.add(80);
+		a.add(60);
+		a.add(50);
+		System.out.println(a);
+		Collections.sort(a);
+		System.out.println(a);
+		
+	}
+}
+		
+	
+
---
+++ src/main1/qq.java
@@ -0,0 +1,13 @@
+package main1;
+
+public class qq {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test test=new Test ();
+		test.ms();
+				
+
+	}
+
+}
